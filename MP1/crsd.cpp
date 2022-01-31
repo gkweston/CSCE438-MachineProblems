24f55812275383e2f1dc1348c373280f9c523fac
gkweston <gkweston@gmail.com>
2022-01-31 16:04:53 -0600

    CREATE working, some JOIN, no message forwarding, no client multiplexing

--- MP1/crsd.cpp
+++ MP1/crsd.cpp
@@ -26,15 +26,21 @@ typedef uint32_t u32;
 #define MAX_MEMBERS (20)
 #define MAX_ROOM	(20)
 
+// (!) mutex on accessing table data
+// (!) mutex on writing port data
+
 typedef struct {
 	char room_name[MAX_NAME];
-	int port_num;
+	int room_sock;
+	int room_port;
 	int num_members;
 	int member_socks[MAX_MEMBERS];
+	pthread_mutex_t room_mutex;
 } room;
 
-room room_table[MAX_ROOM];
-int num_rooms = 0;
+// Let this carry pointers so we can easily free what we alloc
+room* room_table[MAX_ROOM];
+int n_rooms = 0;
 
 pthread_mutex_t room_table_mutex;
 
@@ -50,14 +56,28 @@ pthread_mutex_t room_table_mutex;
 void* connection_handler(void*);
 void* chat_handler(void*);
 
+void make_new_room(char* name, int port, int sock) {
+
+	printf("Adding:\n%s %d %d", name, port, sock);
+
+	room* new_room = new room;
+	strcpy(new_room->room_name, name);
+	new_room->room_sock = sock;
+	new_room->room_port = port;
+	new_room->num_members = 0;
+
+	room_table[n_rooms] = new_room;
+	n_rooms += 1;
+}
+
 void int_to_bytearray(char* ba, int* i) {
 	/* (!) test on c9 */
 	memcpy(ba, i, 4);
 }
 
 int room_exists(char* name) {
-	for (int i = 0; i < num_rooms; i++) {
-		if (strcmp(room_table[i].room_name, name) == 0) {
+	for (int i = 0; i < n_rooms; i++) {
+		if (strcmp(room_table[i]->room_name, name) == 0) {
 			return 1;
 		}
 	}
@@ -128,26 +148,57 @@ int main(int argc , char *argv[]) {
 	return 0;
 }
 
-int handle_CREATE(int sock, char* name) {
-	char resp[MAX_DATA];
+char handle_CREATE(int master_sock, char* name) {
 
 	// 1. Check if data struct has space
 	// 2. Check if room already in data struct
-	pthread_mutex_lock(&room_table_mutex);
-	if (num_rooms >= MAX_ROOM || room_exists(name)) {
-		resp[0] = (char) FAILURE_INVALID;
-	} else {
-		// 3. Find available port to set aside for chatroom
-		
-
-		// 4. Save to data struct and resp to client
-		
+	if (n_rooms >= MAX_ROOM || room_exists(name)) {
+		return (char) FAILURE_ALREADY_EXISTS;
 	}
 
-	write(sock, resp, MAX_DATA);
-	pthread_mutex_unlock(&room_table_mutex);
+	// 3. Find available port to set aside for chatroom
+	int chat_sock;
+	if ((chat_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		cout << "Failure on chatroom socket init\n";
+		return (char) FAILURE_INVALID;
+	}
+	cout << "Chatroom sock init successful\n";
+	
+	// init serv struct
+	struct sockaddr_in serv_addr;
+	bzero((char*) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = 0;	// auto
 
-	return 1;
+	// try bind auto-selected socket
+	if (bind(chat_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
+		if (errno == EADDRINUSE) {
+			cout << "Failure, chatroom port not available\n";
+			return (char) FAILURE_INVALID;
+		} else {
+			cout << "Failure on chatroom port bind\n";
+			return (char) FAILURE_INVALID;
+		}
+	}
+	cout << "Chatroom port bind done\n";
+
+	// sockname error
+	socklen_t len = sizeof(serv_addr);
+	if (getsockname(chat_sock, (struct sockaddr*) &serv_addr, &len) == -1) {
+		perror("getsockname");
+		return (char) FAILURE_INVALID;
+	}
+
+	// chat_port is network byte order serialized
+	int chat_port = ntohs(serv_addr.sin_port);
+
+	// 4. Save to data struct
+	make_new_room(name, chat_port, chat_sock);
+
+	// 5. Send resp to client
+	cout << "Create success\n";
+	return (char) SUCCESS;
 }
 
 void* chat_handler(void* sockfd) {
@@ -178,72 +229,64 @@ void* chat_handler(void* sockfd) {
 	return 0;
 }
 
-int handle_JOIN(int master_sock) {
-	// (!) goes in CREATE, JOIN will simply fetch the proper port
-	// IMPL mutex on fetching proper port or account numbers
-	// 1. Get new port for chatroom (later we will check/update)
-	// 1* Fetch port from datastruct
-	int chat_sock;
-	if ((chat_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		cout << "Failure on chatroom socket init\n";
-		return -1;
-	}
-	cout << "Chatroom sock init successful\n";
-	
-	// init serv struct
-	struct sockaddr_in serv_addr;
-	bzero((char*) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = 0;	// auto
+void handle_JOIN(int master_sock, char* name) {
+	// IMPL mutex on access table data
+	char resp[MAX_DATA];
+	int chat_port, chat_sock, n_members = -1;
 
-	// try bind auto-selected socket
-	if (bind(chat_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0) {
-		if (errno == EADDRINUSE) {
-			cout << "Failure, chatroom port not available\n";
-			return -1;
-		} else {
-			cout << "Failure on chatroom port bind\n";
-			return -1;
+	// 1. Fetch port from datastruct
+	room* r;
+	for (int i = 0; i < n_rooms; i++) {
+		if (strcmp(room_table[i]->room_name, name) == 0) {
+			r = room_table[i];
+
+			if (r->num_members >= MAX_MEMBERS) {
+				break;
+			}
+
+			chat_port = r->room_port;
+			n_members = r->num_members;
+			chat_sock = r->room_sock;
+			break;
 		}
 	}
-	cout << "Chatroom port bind done\n";
-
-	// sockname error
-	socklen_t len = sizeof(serv_addr);
-	if (getsockname(chat_sock, (struct sockaddr*) &serv_addr, &len) == -1) {
-		perror("getsockname");
-		return -1;
+	if (chat_port == -1) {
+		cout << "Failure room DNE or is full\n";
+		resp[0] = (char) FAILURE_NOT_EXISTS;
+		write(master_sock, resp, 1);
+		return;
 	}
-
-	// chat_port should be network byte order serialized
-	int chat_port = ntohs(serv_addr.sin_port);
-	// int chat_port = serv_addr.sin_port;
-	// check num_members, dbg=42 (!)
 	
 	// 2. Byte order serialize param(s)
-	int tmembers = (int) htonl((u32) 42);
+	n_members = (int) htonl((u32) n_members);
 	chat_port = (int) htonl((u32) chat_port);
 
 	// 3. fill in resp buffer { 1B STATUS | 4B N_MEMBER | 4B PORT }
-	char resp[MAX_DATA];
 	resp[0] = (char) SUCCESS;
-	int_to_bytearray(resp + 1, &tmembers);
+	int_to_bytearray(resp + 1, &n_members);
 	int_to_bytearray(resp + 5, &chat_port);
 
 	// 4. Write resp to client and listen for incoming
 	write(master_sock, resp, MAX_DATA);
-	
 	cout << "Waiting for chatroom connection\n";
 	listen(chat_sock, 3);
 
+	
 	// 5. On connection, spawn a new thread to handle chatroom
 	int size = sizeof(struct sockaddr_in);
 	int client_sock;
 	int* new_sock;
+	int added = 0;
 	struct sockaddr_in client;
 	while((client_sock = accept(chat_sock, (struct sockaddr*) &client, (socklen_t*) &size))) {
 		cout << "Chatroom connection accepted\n";
+
+		if (added == 0) {
+			int n = r->num_members;
+			r->member_socks[n] = client_sock;
+			r->num_members += 1;
+			added = 1;
+		}
 
 		pthread_t s_thread;
 		new_sock = new int;
@@ -251,24 +294,22 @@ int handle_JOIN(int master_sock) {
 
 		if(pthread_create(&s_thread, NULL, chat_handler, (void*) new_sock) < 0) {
 			perror("could not create thread");
-			return 1;
+			return;
 		}
 		
 		// Join thread (!)
 		// pthread_join(s_thread, NULL);
 
 		cout << "Handler assigned\n";
-
 	}
 
-	return 1;
+	return;
 }
 
-/*
- * This will handle connection for each client
- * */
 void *connection_handler(void *socket_desc) {
 	//Get the socket descriptor
+	// pthread_mutex_lock(&room_table_mutex);
+
 	int sock = *(int*)socket_desc;
 
 	// free(socket_desc); exit(1);
@@ -276,34 +317,24 @@ void *connection_handler(void *socket_desc) {
 	int recv_size;
 	char client_message[MAX_DATA], resp[MAX_DATA];
 	char cmd;
+	int send_resp = 1;
 
 	//Receive a message from client
 	while((recv_size = recv(sock, client_message, MAX_DATA , 0)) > 0) {
 		client_message[strlen(client_message)] = '\0';
 		cmd = client_message[0];
 		if (cmd == CREATE) {
-			cout << "CREATE|" << client_message+1 << '\n';
-			resp[0] = (char) SUCCESS;
+			cout << "CREATE|" << client_message + 1;
+			resp[0] = handle_CREATE(sock, client_message + 1);
+
 		} else if (cmd == DELETE) {
 			cout << "DELETE|" << client_message+1 << '\n';
 			resp[0] = (char) SUCCESS;
+
 		} else if (cmd == JOIN) {
 			cout << "JOIN|" << client_message +1 << '\n';
-			
-			/* RAW testing (!)
-			char members_port[8];
-			int tmembers = (int) htonl((u32) 42);
-			int tport = (int) htonl((u32) 777);
-			int_to_bytearray(members_port, &tmembers);
-			int_to_bytearray(members_port + 4, &tport);
-			resp[0] = (char) SUCCESS;
-			memcpy(resp+1, members_port, 8);
-			*/
-			if (handle_JOIN(sock) < 0) {
-				cout << "Failure on handle join\n";
-				exit(1);
-			}
-			return 0;
+			handle_JOIN(sock, client_message + 1);
+			send_resp = 0;
 
 		} else if (cmd == LIST) {
 			cout << "LIST\n";
@@ -311,18 +342,13 @@ void *connection_handler(void *socket_desc) {
 			const char* lstring = "room1, room2, room3, room4";
 			int len = strlen(lstring) + 1;
 			strlcpy(resp+1, lstring, len);
-		}
-		
-		
-		else {
-			cout << "$ <" << client_message << ">\n";
-			cout << "L <" << strlen(client_message) << ">\n";
-			cout << "<<< echoing buffer\n";
-			write(sock, client_message , strlen(client_message));
-		}
+
+		} else cout << "Unknown connection command\n";
+
 		
 		// Send response buffer
-		write(sock, resp, MAX_DATA);
+		if (send_resp)
+			write(sock, resp, MAX_DATA);
 	}
 	
 	if(recv_size == 0) {
@@ -335,6 +361,8 @@ void *connection_handler(void *socket_desc) {
 	//Free the socket pointer
 	free(socket_desc);
 	
+	pthread_mutex_unlock(&room_table_mutex);
+
 	return 0;
 }
 
